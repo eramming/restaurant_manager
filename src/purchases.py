6e0d014ee19f1e49a5100288162ddcd0d9c203e0
Eric Ramming <eramming14@gmail.com>
2026-06-09 13:21:27 -0400

    more purchase api fixes

--- src/purchases.py
+++ src/purchases.py
@@ -17,29 +17,30 @@ inventory_table: Table = dynamodb.Table(INVENTORY_TABLE)
 def handle_new_purchases(purchase: PurchasedIngrs):
     for ingredient in purchase.ingredients:
         ingr_key = ingredient.ingredient.lower()
+        update_expression = """
+            ADD quantity :quantity
+            SET unit = :unit,
+                latest_price = :latest_price
+        """
+        expression_values = {
+            ":quantity": ingredient.quantity,
+            ":unit": ingredient.unit,
+            ":latest_price": ingredient.latest_price,
+        }
+
+        if ingredient.expiration_date:
+            update_expression += ", expiration_date = :expiration_date"
+            expression_values[":expiration_date"] = ingredient.expiration_date.isoformat()
 
         try:
             inventory_table.update_item(
                 Key={"ingredient": ingr_key},
-                UpdateExpression="""
-                    ADD quantity :quantity
-                    SET unit = :unit,
-                        expiration_date = :expiration_date,
-                        latest_price = :latest_price
-                """,
-                ExpressionAttributeValues={
-                    ":quantity": ingredient.quantity,
-                    ":unit": ingredient.unit,
-                    ":expiration_date": (
-                        ingredient.expiration_date.isoformat()
-                        if ingredient.expiration_date
-                        else None
-                    ),
-                    ":latest_price": ingredient.latest_price
-                }
+                UpdateExpression=update_expression,
+                ExpressionAttributeValues=expression_values
             )
             LOG.info(f"Added {ingredient.quantity} units of {ingr_key} to inventory.")
         except ClientError:
+            LOG.error(f"Failed to update ingredient: {ingr_key} with params {expression_values}.")
             raise HTTPException(status_code=500, detail=f"Failed to update ingredient: {ingr_key}")
 
     return {
